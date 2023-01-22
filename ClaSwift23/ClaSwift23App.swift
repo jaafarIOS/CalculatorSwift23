a211813dad83a279b4eddb13a7053fa4de7d2199
Jaafar Zubaidi <jaafarwerdi@proton.me>
2023-01-22 10:40:26 -0500

    Initial Commit

---
+++ ClaSwift23/ClaSwift23App.swift
@@ -0,0 +1,17 @@
+//
+//  ClaSwift23App.swift
+//  ClaSwift23
+//
+//  Created by Consultant on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ClaSwift23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
